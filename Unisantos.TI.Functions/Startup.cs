5ab237994c2009cf291966a0ecca1e085595d912
Rafael Mello <rafaelmello0715@outlook.com>
2022-10-26 18:01:57 -0300

    Estrutura Inicial

---
+++ Unisantos.TI.Functions/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Unisantos.TI.Functions.Startup))]
+namespace Unisantos.TI.Functions;
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        //builder.Services.AddProviders();
+
+        //builder.Services.AddUseCases();
+    }
+}
