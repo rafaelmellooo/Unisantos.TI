3545efbe571017c940c6244a95444816bf00e722
RenatoDevCarvalho <renatoscarvalho18@gmail.com>
2022-11-17 08:37:04 -0300

    FrontEnd inicial e tela de login

---
+++ frontend/pages/login.tsx
@@ -0,0 +1,55 @@
+import { AccountCircle, VisibilityOff } from '@mui/icons-material'
+import { Button, InputAdornment, TextField } from '@mui/material'
+import Image from 'next/image'
+import Logo from '../public/Logo.png'
+
+export default function Home() {
+    return (
+        <>
+            <Image
+                className='logo-box'
+                src={Logo}
+                alt="logo"
+            />
+
+            <div className="login-box">
+                <TextField
+                    label="Usuário"
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <AccountCircle />
+                            </InputAdornment>
+                        )
+                        ,
+                    }}
+                    variant="standard"
+                    className="login-input"
+                />
+
+                <TextField
+                    label="Senha"
+                    type="password"
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <VisibilityOff />
+                            </InputAdornment>
+                        )
+                        ,
+                    }}
+                    variant="standard"
+                    className="login-input"
+                />
+
+                <Button variant="contained" className="login-button">
+                    ENTRAR
+                </Button>
+
+                <Button variant="contained" className="login-button">
+                    CADASTRE-SE
+                </Button>
+            </div>
+        </>
+    )
+}
