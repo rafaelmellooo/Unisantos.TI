037e1d5a946624c321399ccdc77eacba3eecffff
RenatoDevCarvalho <renatoscarvalho18@gmail.com>
2022-11-27 16:02:24 -0300

    temp

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,7 @@
-import '../styles/globals.css'
-import '../styles/login.css'
+import '../styles/globals.sass'
+import '../styles/login.sass'
+import '../styles/map.sass'
+import '../components/sideMenu.sass'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
